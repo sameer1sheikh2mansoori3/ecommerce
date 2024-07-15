e10f9e0513ccbafdeedbf1d5de8b5b156b3ac9dc
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-07-15 20:50:40 +0530

    Handling uncaugth exception , promise rejection , mongodbId error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDb = require("./config/database");
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
 dotenv.config({
   path: "backend/config/.env",
@@ -9,6 +15,16 @@ console.log(process.env.PORT);
 // const server =
 
 connectDb();
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
