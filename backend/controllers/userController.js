194a862e32b13f2815d07a9739b39a444f1bdb65
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-07-16 14:38:02 +0530

    login signup and logout done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 exports.registerUser = catchAsyncErrors(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -17,5 +18,30 @@ exports.registerUser = catchAsyncErrors(async (req, res) => {
     message: "User created successfully",
     payload: createdUser,
     success: true,
+  });
+});
+
+exports.loginUser = catchAsyncErrors(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email & Password", 400));
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(new ErrorHander("no user found"), 501);
+  }
+
+  sendToken(user, 200, res);
+  return res;
+});
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
   });
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,14 @@
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/userController");
 const express = require("express");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/registerUser").post(registerUser);
+router.route("/login").post(isAuthenticatedUser, loginUser);
+router.route("/logout").post(isAuthenticatedUser, logout);
 
 module.exports = router;
